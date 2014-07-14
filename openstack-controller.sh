15f142cc50f512d882a738b1deb84249e498e023
Flavio Elawi <flavio.elawi@riversman.com>
2014-07-14 16:02:07 +0200

    moved to functions

--- openstack-controller.sh
+++ openstack-controller.sh
@@ -4,75 +4,10 @@ openstack_txt_pwd=openstack_pwd.txt
 keystone_conf="/etc/keystone/keystone.conf"
 
 
-# NODE ENVIRONMENT CONFIGURATION START
-
-apt update
-
-echo "Controller node script"
-echo "input the hostname: "
-read hostname_var
-echo $hostname_var > /etc/hostname
-
-# MYSQL INSTALLATION
-
-if [ -e db_installed ]; then
-	echo "DB Already configured"
-	else
-		#CALL CREATE_DB Function
-		create_db
-fi
-
-touch db_installed
-
-# END MYSQL INSTALLATION
-
-# KEYSTONE INSTALLATION
-
-echo "Installing Openstack keystone server"
-
-apt install -y keystone python-keystone python-keystoneclient
-
-echo "Configuring Keystone config file"
-admin_token_var=$(openssl rand -base64 32)
-echo "Admin token password: $admin_token_var" >> $openstack_txt_pwd
-if [ -e /etc/keystone/keystone.conf.bak ]; then
-	echo "backup file already exists, probably Keystone is already configured"
-	else
-		# call configure_keystone function
-		configure_keystone
-		# BEGIN KEYSTONE CONFIGURATION
-
-		echo "export OS_USERNAME=admin" > openrc
-		echo "export OS_PASSWORD=$admin_token_var" >> openrc
-		echo "export OS_TENANT_NAME=admin" >> openrc
-		echo "export OS_AUTH_URL=http://localhost:35357/v2.0" >> openrc
-		source openrc
-		echo "source openrc" >> .bashrc
-
-		echo "Insert the admin user password " 
-		read -s admin_pass
-		echo "Admin password: $admin_pass" >> $openstack_txt_pwd
-		echo "Insert the admin email address "
-		read -s admin_email
-		echo "Admin email: $admin_email" >> $openstack_txt_pwd
-
-		keystone user-create --name=admin --pass=$admin_pass --email=$admin_email
-		keystone role-create --name=admin
-		keystone tenant-create --name=admin --description="Admin Tenant"
-		keystone user-role-add --user=admin --tenant=admin --role=admin
-		keystone user-role-add --user=admin --role=_member_ --tenant=admin
-		keystone tenant-create --name=service --description="Service Tenant"
-		keystone service-create --name=keystone --type=identity --description="OpenStack Identity"
-
-		# END KEYSTONE CONFIGURATION
-fi
-
-# END KEYSTONE INSTALLATION
-
 # FUNCTIONS
 
-function create_db
-{
+function create_db(){
+
 	apt install -y python-mysqldb mysql-server rabbitmq-server
 	rabbit_pass=$(openssl rand -base64 32)	
 	echo "RabbitMQ guest password: $rabbit_pass" > openstack_pwd.txt
@@ -115,9 +50,10 @@ function create_db
 	GRANT ALL PRIVILEGES ON keystone.* TO "keystone"@"%" IDENTIFIED BY "$password_db_keystone";
 	FLUSH PRIVILEGES;
 EOF
+touch db_installed
 }
 
-function configure_keystone {
+function configure_keystone() {
 
 	echo "Creating backup config file (Keystone) "
 	cp $keystone_conf /etc/keystone/keystone.conf.bak
@@ -127,4 +63,72 @@ function configure_keystone {
 	keystone-manage db_sync
 	service keystone restart
 }
+
+
+# NODE ENVIRONMENT CONFIGURATION START
+
+apt update
+
+echo "Controller node script"
+echo "input the hostname: "
+read hostname_var
+echo $hostname_var > /etc/hostname
+
+# MYSQL INSTALLATION
+
+if [ -e db_installed ]; then
+	echo "DB Already configured"
+	else
+		#CALL CREATE_DB Function
+		create_db;
+fi
+
+
+
+# END MYSQL INSTALLATION
+
+# KEYSTONE INSTALLATION
+
+echo "Installing Openstack keystone server"
+
+apt install -y keystone python-keystone python-keystoneclient
+
+echo "Configuring Keystone config file"
+admin_token_var=$(openssl rand -base64 32)
+echo "Admin token password: $admin_token_var" >> $openstack_txt_pwd
+if [ -e /etc/keystone/keystone.conf.bak ]; then
+	echo "backup file already exists, probably Keystone is already configured"
+	else
+		# call configure_keystone function
+		configure_keystone ;
+		# BEGIN KEYSTONE CONFIGURATION
+
+		echo "export OS_USERNAME=admin" > openrc
+		echo "export OS_PASSWORD=$admin_token_var" >> openrc
+		echo "export OS_TENANT_NAME=admin" >> openrc
+		echo "export OS_AUTH_URL=http://localhost:35357/v2.0" >> openrc
+		source openrc
+		echo "source openrc" >> .bashrc
+
+		echo "Insert the admin user password " 
+		read -s admin_pass
+		echo "Admin password: $admin_pass" >> $openstack_txt_pwd
+		echo "Insert the admin email address "
+		read -s admin_email
+		echo "Admin email: $admin_email" >> $openstack_txt_pwd
+
+		keystone user-create --name=admin --pass=$admin_pass --email=$admin_email
+		keystone role-create --name=admin
+		keystone tenant-create --name=admin --description="Admin Tenant"
+		keystone user-role-add --user=admin --tenant=admin --role=admin
+		keystone user-role-add --user=admin --role=_member_ --tenant=admin
+		keystone tenant-create --name=service --description="Service Tenant"
+		keystone service-create --name=keystone --type=identity --description="OpenStack Identity"
+
+		# END KEYSTONE CONFIGURATION
+fi
+
+# END KEYSTONE INSTALLATION
+
+
 
